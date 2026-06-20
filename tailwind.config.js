fabcfec63aab3909f9ab87254de9d3ff5e790637
John Mark Delima <delimajohnmark14@gmail.com>
2026-06-20 12:58:47 +0800

    Update new design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
-  presets: [require('nativewind/preset')],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vibe-green': '#5E715B',
+        'art-sand': '#D1BFA5',
+        'twilight-maroon': '#5A3A31',
+        'dark-charcoal': '#1E2326',
+        'soft-cream': '#F4F1EA',
+      },
+      fontFamily: {
+        // The clean font for the buttons
+        sans: ['Inter_400Regular', 'sans-serif'],
+        // The cinematic fonts for the title
+        serif: ['EBGaramond_400Regular', 'serif'],
+        serifItalic: ['EBGaramond_400Regular_Italic', 'serif'],
+      }
+    },
   },
   plugins: [],
-};
+}
