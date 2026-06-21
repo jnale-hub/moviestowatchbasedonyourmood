8b363bf9133090703cc524bf29b87a1d77bc0fbc
John Mark Delima <delimajohnmark14@gmail.com>
2026-06-21 17:26:24 +0800

    Add supabase feature

---
+++ src/lib/supabase.ts
@@ -0,0 +1,18 @@
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Supabase Error: Missing environment variables! Check your EXPO_PUBLIC_ prefixes.");
+}
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
